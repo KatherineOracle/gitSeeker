13ba211cacb1376fcaaa43a3efc45f653e7ec1d1
KatherineOracle <katherine@rasc.co.za>
2022-08-28 15:24:39 +0200

    CSP update

--- index.js
+++ index.js
@@ -8,7 +8,29 @@ const config = require('dotenv').config()
 const path = require('path')
 const express = require("express");
 const helmet = require("helmet");
-const cors = require("cors");
+
+
+const allowlist = ['https://avatars.githubusercontent.com', 'https://secure.gravatar.com/'];
+
+    const corsOptionsDelegate = (req, callback) => {
+    let corsOptions;
+
+    let isDomainAllowed = whitelist.indexOf(req.header('Origin')) !== -1;
+
+
+    if (isDomainAllowed && isExtensionAllowed) {
+        // Enable CORS for this request
+        corsOptions = { origin: true }
+    } else {
+        // Disable CORS for this request
+        corsOptions = { origin: false }
+    }
+    callback(null, corsOptions)
+}
+
+app.use(cors(corsOptionsDelegate));
+
+
 const routes = require("./routes");
 
 const PORT = process.env.PORT || 5000;
@@ -17,6 +39,8 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(helmet());
 app.use(express.json());
+
+
 app.use(cors());
 app.use("/api", routes);
 
