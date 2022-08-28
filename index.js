bf14da04b8bdcaccaf19402d2b8299eaf5583a16
KatherineOracle <katherine@rasc.co.za>
2022-08-28 15:54:58 +0200

    CSP update

--- index.js
+++ index.js
@@ -8,28 +8,7 @@ const config = require('dotenv').config()
 const path = require('path')
 const express = require("express");
 const helmet = require("helmet");
-
-
-const allowlist = ['https://avatars.githubusercontent.com', 'https://secure.gravatar.com/'];
-
-  const corsOptionsDelegate = (req, callback) => {
-    let corsOptions;
-
-    let isDomainAllowed = whitelist.indexOf(req.header('Origin')) !== -1;
-
-    if (isDomainAllowed) {
-        // Enable CORS for this request
-        corsOptions = { origin: true }
-    } else {
-        // Disable CORS for this request
-        corsOptions = { origin: false }
-    }
-    callback(null, corsOptions)
-}
-
-app.use(cors(corsOptionsDelegate));
-
-
+const cors = require("cors");
 const routes = require("./routes");
 
 const PORT = process.env.PORT || 5000;
@@ -45,8 +24,10 @@ app.use("/api", routes);
 
 if (process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*',(req,res)=> {res.sendFile(path.resolve(__dirname,
-  'client', 'build','index.html'));
+  app.get('*',(req,res)=> {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'; img-src 'self' https://avatars.githubusercontent.com https://secure.gravatar.com;");
+    res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
   });
   }    
   
