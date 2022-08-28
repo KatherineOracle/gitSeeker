dc15a3822352a4cdf891930e1ce3cc16576aec2e
KatherineOracle <katherine@rasc.co.za>
2022-08-28 14:22:19 +0200

    Heroku build

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ Get users from Github and Gitlabs
 
 // importing the dependencies
 const config = require('dotenv').config()
+const path = require('path')
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const routes = require("./routes");
+
 const PORT = process.env.PORT || 5000;
 
 // defining the Express app
@@ -18,6 +20,13 @@ app.use(express.json());
 app.use(cors());
 app.use("/api", routes);
 
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*',(req,res)=> {res.sendFile(path.resolve(__dirname,
+  'client', 'build','index.html'));
+  });
+  }    
+  
 
 /* start up the API server on port 5000!*/
 app.listen(PORT, () => {
